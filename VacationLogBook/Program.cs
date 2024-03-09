6528d87c55bebc5d89289af4a7fcbbf5073a34fc
Alex <minino-forever@rambler.ru>
2024-03-09 22:36:01 +0700

    Добавьте файлы проекта.

---
+++ VacationLogBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VacationLogBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
