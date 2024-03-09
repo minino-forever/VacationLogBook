0c92a84fe37d36a465a3d7910484766f8b54ce00
Alex <minino-forever@rambler.ru>
2024-03-09 23:36:01 +0700

    added propertiesPosition

--- VacationLogBook/Position.cs
+++ VacationLogBook/Position.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace VacationLogBook
 {
+    /// <summary>Должности</summary>
     class Position
     {
+        /// <summary>Электрик</summary>
         public string Electrician { get; set; }
+        /// <summary>Слесарь-инструментальщик</summary>
+        public string Toolmaker { get; set; }
+        /// <summary>Слесарь-сборщик</summary>
+        public string Fitter { get; set; }
+        /// <summary>\Слесарь-ремонтник</summary>
+        public string Repairer { get; set; }
+        /// <summary>Наладчик оборудования</summary>
+        public string Turner { get; set; }
+        /// <summary>Токарь</summary>
+
     }
 }
